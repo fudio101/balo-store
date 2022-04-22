c742065f6243cab5c9c788fbc613381e258a99c6
Nguyễn Đỗ Thế Nguyên <thenguyen1024@gmail.com>
2022-04-22 14:59:58 +0700

    update database structure

--- database/migrations/2014_10_11_053652_create_user_groups_table.php
+++ database/migrations/2014_10_11_053652_create_user_groups_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
         Schema::create('user_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
 
