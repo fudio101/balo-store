b2310918cde258345269710ce3fd8c58b9cdda5d
Nguyễn Đỗ Thế Nguyên <thenguyen1024@gmail.com>
2022-05-04 11:02:43 +0700

    faker data order table

--- database/migrations/dvhcvn.sql
+++ database/migrations/dvhcvn.sql
@@ -1,28 +1,69 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: balostore
--- ------------------------------------------------------
--- Server version	8.0.28
+INSERT INTO provinces
+VALUES (1, 'Thành phố Hà Nội', 'Thành phố Trung ương'),
+       (2, 'Tỉnh Hà Giang', 'Tỉnh'),
+       (4, 'Tỉnh Cao Bằng', 'Tỉnh'),
+       (6, 'Tỉnh Bắc Kạn', 'Tỉnh'),
+       (8, 'Tỉnh Tuyên Quang', 'Tỉnh'),
+       (10, 'Tỉnh Lào Cai', 'Tỉnh'),
+       (11, 'Tỉnh Điện Biên', 'Tỉnh'),
+       (12, 'Tỉnh Lai Châu', 'Tỉnh'),
+       (14, 'Tỉnh Sơn La', 'Tỉnh'),
+       (15, 'Tỉnh Yên Bái', 'Tỉnh'),
+       (17, 'Tỉnh Hoà Bình', 'Tỉnh'),
+       (19, 'Tỉnh Thái Nguyên', 'Tỉnh'),
+       (20, 'Tỉnh Lạng Sơn', 'Tỉnh'),
+       (22, 'Tỉnh Quảng Ninh', 'Tỉnh'),
+       (24, 'Tỉnh Bắc Giang', 'Tỉnh'),
+       (25, 'Tỉnh Phú Thọ', 'Tỉnh'),
+       (26, 'Tỉnh Vĩnh Phúc', 'Tỉnh'),
+       (27, 'Tỉnh Bắc Ninh', 'Tỉnh'),
+       (30, 'Tỉnh Hải Dương', 'Tỉnh'),
+       (31, 'Thành phố Hải Phòng', 'Thành phố Trung ương'),
+       (33, 'Tỉnh Hưng Yên', 'Tỉnh'),
+       (34, 'Tỉnh Thái Bình', 'Tỉnh'),
+       (35, 'Tỉnh Hà Nam', 'Tỉnh'),
+       (36, 'Tỉnh Nam Định', 'Tỉnh'),
+       (37, 'Tỉnh Ninh Bình', 'Tỉnh'),
+       (38, 'Tỉnh Thanh Hóa', 'Tỉnh'),
+       (40, 'Tỉnh Nghệ An', 'Tỉnh'),
+       (42, 'Tỉnh Hà Tĩnh', 'Tỉnh'),
+       (44, 'Tỉnh Quảng Bình', 'Tỉnh'),
+       (45, 'Tỉnh Quảng Trị', 'Tỉnh'),
+       (46, 'Tỉnh Thừa Thiên Huế', 'Tỉnh'),
+       (48, 'Thành phố Đà Nẵng', 'Thành phố Trung ương'),
+       (49, 'Tỉnh Quảng Nam', 'Tỉnh'),
+       (51, 'Tỉnh Quảng Ngãi', 'Tỉnh'),
+       (52, 'Tỉnh Bình Định', 'Tỉnh'),
+       (54, 'Tỉnh Phú Yên', 'Tỉnh'),
+       (56, 'Tỉnh Khánh Hòa', 'Tỉnh'),
+       (58, 'Tỉnh Ninh Thuận', 'Tỉnh'),
+       (60, 'Tỉnh Bình Thuận', 'Tỉnh'),
+       (62, 'Tỉnh Kon Tum', 'Tỉnh'),
+       (64, 'Tỉnh Gia Lai', 'Tỉnh'),
+       (66, 'Tỉnh Đắk Lắk', 'Tỉnh'),
+       (67, 'Tỉnh Đắk Nông', 'Tỉnh'),
+       (68, 'Tỉnh Lâm Đồng', 'Tỉnh'),
+       (70, 'Tỉnh Bình Phước', 'Tỉnh'),
+       (72, 'Tỉnh Tây Ninh', 'Tỉnh'),
+       (74, 'Tỉnh Bình Dương', 'Tỉnh'),
+       (75, 'Tỉnh Đồng Nai', 'Tỉnh'),
+       (77, 'Tỉnh Bà Rịa - Vũng Tàu', 'Tỉnh'),
+       (79, 'Thành phố Hồ Chí Minh', 'Thành phố Trung ương'),
+       (80, 'Tỉnh Long An', 'Tỉnh'),
+       (82, 'Tỉnh Tiền Giang', 'Tỉnh'),
+       (83, 'Tỉnh Bến Tre', 'Tỉnh'),
+       (84, 'Tỉnh Trà Vinh', 'Tỉnh'),
+       (86, 'Tỉnh Vĩnh Long', 'Tỉnh'),
+       (87, 'Tỉnh Đồng Tháp', 'Tỉnh'),
+       (89, 'Tỉnh An Giang', 'Tỉnh'),
+       (91, 'Tỉnh Kiên Giang', 'Tỉnh'),
+       (92, 'Thành phố Cần Thơ', 'Thành phố Trung ương'),
+       (93, 'Tỉnh Hậu Giang', 'Tỉnh'),
+       (94, 'Tỉnh Sóc Trăng', 'Tỉnh'),
+       (95, 'Tỉnh Bạc Liêu', 'Tỉnh'),
+       (96, 'Tỉnh Cà Mau', 'Tỉnh');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
-
---
--- Dumping data for table `districts`
---
-
-LOCK TABLES `districts` WRITE;
-/*!40000 ALTER TABLE `districts`
-    DISABLE KEYS */;
-INSERT INTO `districts`
+INSERT INTO districts
 VALUES (1, 'Quận Ba Đình', 'Quận', 1),
        (2, 'Quận Hoàn Kiếm', 'Quận', 1),
        (3, 'Quận Tây Hồ', 'Quận', 1),
@@ -477,7 +518,7 @@ VALUES (1, 'Quận Ba Đình', 'Quận', 1),
        (615, 'Huyện Đắk Hà', 'Huyện', 62),
        (616, 'Huyện Sa Thầy', 'Huyện', 62),
        (617, 'Huyện Tu Mơ Rông', 'Huyện', 62),
-       (618, 'Huyện Ia H\' Drai', 'Huyện', 62),
+       (618, 'Huyện Ia H\''Drai', 'Huyện', 62),
        (622, 'Thành phố Pleiku', 'Thành phố', 64),
        (623, 'Thị xã An Khê', 'Thị xã', 64),
        (624, 'Thị xã Ayun Pa', 'Thị xã', 64),
@@ -497,14 +538,14 @@ VALUES (1, 'Quận Ba Đình', 'Quận', 1),
        (639, 'Huyện Chư Pưh', 'Huyện', 64),
        (643, 'Thành phố Buôn Ma Thuột', 'Thành phố', 66),
        (644, 'Thị Xã Buôn Hồ', 'Thị xã', 66),
-       (645, 'Huyện Ea H\'leo', 'Huyện', 66),
+       (645, 'Huyện Ea H\''leo', 'Huyện', 66),
        (646, 'Huyện Ea Súp', 'Huyện', 66),
        (647, 'Huyện Buôn Đôn', 'Huyện', 66),
-       (648, 'Huyện Cư M\'gar', 'Huyện', 66),
+       (648, 'Huyện Cư M\''gar', 'Huyện', 66),
        (649, 'Huyện Krông Búk', 'Huyện', 66),
        (650, 'Huyện Krông Năng', 'Huyện', 66),
        (651, 'Huyện Ea Kar', 'Huyện', 66),
-       (652, 'Huyện M\'Đrắk', 'Huyện', 66),
+       (652, 'Huyện M\''Đrắk', 'Huyện', 66),
        (653, 'Huyện Krông Bông', 'Huyện', 66),
        (654, 'Huyện Krông Pắc', 'Huyện', 66),
        (655, 'Huyện Krông A Na', 'Huyện', 66),
@@ -736,92 +777,3 @@ VALUES (1, 'Quận Ba Đình', 'Quận', 1),
        (971, 'Huyện Năm Căn', 'Huyện', 96),
        (972, 'Huyện Phú Tân', 'Huyện', 96),
        (973, 'Huyện Ngọc Hiển', 'Huyện', 96);
-/*!40000 ALTER TABLE `districts`
-    ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `provinces`
---
-
-LOCK TABLES `provinces` WRITE;
-/*!40000 ALTER TABLE `provinces`
-    DISABLE KEYS */;
-INSERT INTO `provinces`
-VALUES (1, 'Thành phố Hà Nội', 'Thành phố Trung ương'),
-       (2, 'Tỉnh Hà Giang', 'Tỉnh'),
-       (4, 'Tỉnh Cao Bằng', 'Tỉnh'),
-       (6, 'Tỉnh Bắc Kạn', 'Tỉnh'),
-       (8, 'Tỉnh Tuyên Quang', 'Tỉnh'),
-       (10, 'Tỉnh Lào Cai', 'Tỉnh'),
-       (11, 'Tỉnh Điện Biên', 'Tỉnh'),
-       (12, 'Tỉnh Lai Châu', 'Tỉnh'),
-       (14, 'Tỉnh Sơn La', 'Tỉnh'),
-       (15, 'Tỉnh Yên Bái', 'Tỉnh'),
-       (17, 'Tỉnh Hoà Bình', 'Tỉnh'),
-       (19, 'Tỉnh Thái Nguyên', 'Tỉnh'),
-       (20, 'Tỉnh Lạng Sơn', 'Tỉnh'),
-       (22, 'Tỉnh Quảng Ninh', 'Tỉnh'),
-       (24, 'Tỉnh Bắc Giang', 'Tỉnh'),
-       (25, 'Tỉnh Phú Thọ', 'Tỉnh'),
-       (26, 'Tỉnh Vĩnh Phúc', 'Tỉnh'),
-       (27, 'Tỉnh Bắc Ninh', 'Tỉnh'),
-       (30, 'Tỉnh Hải Dương', 'Tỉnh'),
-       (31, 'Thành phố Hải Phòng', 'Thành phố Trung ương'),
-       (33, 'Tỉnh Hưng Yên', 'Tỉnh'),
-       (34, 'Tỉnh Thái Bình', 'Tỉnh'),
-       (35, 'Tỉnh Hà Nam', 'Tỉnh'),
-       (36, 'Tỉnh Nam Định', 'Tỉnh'),
-       (37, 'Tỉnh Ninh Bình', 'Tỉnh'),
-       (38, 'Tỉnh Thanh Hóa', 'Tỉnh'),
-       (40, 'Tỉnh Nghệ An', 'Tỉnh'),
-       (42, 'Tỉnh Hà Tĩnh', 'Tỉnh'),
-       (44, 'Tỉnh Quảng Bình', 'Tỉnh'),
-       (45, 'Tỉnh Quảng Trị', 'Tỉnh'),
-       (46, 'Tỉnh Thừa Thiên Huế', 'Tỉnh'),
-       (48, 'Thành phố Đà Nẵng', 'Thành phố Trung ương'),
-       (49, 'Tỉnh Quảng Nam', 'Tỉnh'),
-       (51, 'Tỉnh Quảng Ngãi', 'Tỉnh'),
-       (52, 'Tỉnh Bình Định', 'Tỉnh'),
-       (54, 'Tỉnh Phú Yên', 'Tỉnh'),
-       (56, 'Tỉnh Khánh Hòa', 'Tỉnh'),
-       (58, 'Tỉnh Ninh Thuận', 'Tỉnh'),
-       (60, 'Tỉnh Bình Thuận', 'Tỉnh'),
-       (62, 'Tỉnh Kon Tum', 'Tỉnh'),
-       (64, 'Tỉnh Gia Lai', 'Tỉnh'),
-       (66, 'Tỉnh Đắk Lắk', 'Tỉnh'),
-       (67, 'Tỉnh Đắk Nông', 'Tỉnh'),
-       (68, 'Tỉnh Lâm Đồng', 'Tỉnh'),
-       (70, 'Tỉnh Bình Phước', 'Tỉnh'),
-       (72, 'Tỉnh Tây Ninh', 'Tỉnh'),
-       (74, 'Tỉnh Bình Dương', 'Tỉnh'),
-       (75, 'Tỉnh Đồng Nai', 'Tỉnh'),
-       (77, 'Tỉnh Bà Rịa - Vũng Tàu', 'Tỉnh'),
-       (79, 'Thành phố Hồ Chí Minh', 'Thành phố Trung ương'),
-       (80, 'Tỉnh Long An', 'Tỉnh'),
-       (82, 'Tỉnh Tiền Giang', 'Tỉnh'),
-       (83, 'Tỉnh Bến Tre', 'Tỉnh'),
-       (84, 'Tỉnh Trà Vinh', 'Tỉnh'),
-       (86, 'Tỉnh Vĩnh Long', 'Tỉnh'),
-       (87, 'Tỉnh Đồng Tháp', 'Tỉnh'),
-       (89, 'Tỉnh An Giang', 'Tỉnh'),
-       (91, 'Tỉnh Kiên Giang', 'Tỉnh'),
-       (92, 'Thành phố Cần Thơ', 'Thành phố Trung ương'),
-       (93, 'Tỉnh Hậu Giang', 'Tỉnh'),
-       (94, 'Tỉnh Sóc Trăng', 'Tỉnh'),
-       (95, 'Tỉnh Bạc Liêu', 'Tỉnh'),
-       (96, 'Tỉnh Cà Mau', 'Tỉnh');
-/*!40000 ALTER TABLE `provinces`
-    ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
-
--- Dump completed on 2022-05-04  9:19:32
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Discount;
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        if (env('APP_URL') == 'http://localhost') {
+            DB::unprepared(file_get_contents('database/migrations/dvhc_mysql.sql'));
+        } else {
+            DB::unprepared(file_get_contents('database/migrations/dvhcvn.sql'));
+        }
         Category::factory(7)->create();
         Product::factory(20)->create();
         Discount::factory(20)->create();
+        Order::factory(25)->create();
 
     }
 }
